27af8893a1c085d0fd47ee792265b984c0d7507a
KieronGillingham <KieronGillingham@gmail.com>
2021-03-12 10:24:38 +0000

    remove old stt config function

--- prototypes/OpenAssistant_DeepSpeech/oa/modules/speech_recognition/__init__.py
+++ prototypes/OpenAssistant_DeepSpeech/oa/modules/speech_recognition/__init__.py
@@ -15,41 +15,6 @@ from oa.modules.abilities.system import download_file, write_file, stat_mtime
 _decoders = {}
 _logger = logging.getLogger(__name__)
 
-
-def config_stt(cache_dir, keywords, kws_last_modification_time_in_sec=None):
-    _ = oa.legacy.Core()
-    cache_path = lambda x: os.path.join(cache_dir, x)
-    _.lang_file = cache_path('lm')
-    _.fsg_file = None
-    _.dic_file = cache_path('dic')
-    _.hmm_dir = cache_path('hmm')
-
-    # Save keywords for further pattern matching. Call `config_stt()` when switching minds to check for command file changes.
-    _.kwords = {}
-    _.max_w_cnt = 3
-    for phrase in keywords:
-        spl_ph = phrase.strip().split(' ')
-        w_cnt = len(spl_ph)
-        if w_cnt > _.max_w_cnt:
-            _.max_w_cnt = w_cnt
-        for kword in spl_ph:
-            # Combine all phrases which use keywords.
-            r_phrases = _.kwords.setdefault(kword,{})
-            r_phrases[phrase] = w_cnt
-
-    # Save phrases.
-    _.phrases = [x.strip().replace('%d', '').upper() for x in sorted(keywords)]
-
-    # Check if commands file was modified.
-    if kws_last_modification_time_in_sec:
-        if os.path.exists(_.dic_file) and (kws_last_modification_time_in_sec < stat_mtime(_.dic_file)):
-            return _
-    _.strings_file = cache_path("sentences.corpus")
-    data = '\n'.join(_.phrases)
-    write_file(_.strings_file, data)
-
-    return _
-
 def get_decoder():
     # XXX: race condition when mind isn't set yet
     mind = oa.legacy.mind
