657e175ab91e5b12f305d80a209f1f9307707a9a
Kieron Gillingham <KieronGillingham@gmail.com>
2021-04-16 10:55:28 +0100

    levels of gameplay

--- dementiaVA/oa/modules/abilities/highlow.py
+++ dementiaVA/oa/modules/abilities/highlow.py
@@ -1,22 +1,29 @@
-from itertools import groupby
-
-import oa.legacy
-
+import logging
 import random
 from time import sleep
 
-from oa.modules.abilities.core import info
 from oa.modules.abilities.interact import say, user_answer, play
+
+_logger = logging.getLogger(__name__)
 
 class Game:
     current_number = None
     last_number = None
     score = 0
     round = 0
+    level = 1
+    streak = 0
+    # Multiple levels of difficulty
+    # Level 1: Numbers 1-10
+    # Level 2: Numbers 1-20
+    # Level 3: Numbers 1-20, with gaps between numbers
+    # Level 4: Numbers 1-50, with gaps between numbers
+    # Level 5: Numbers 1-100, with gaps between numbers
+
 
     def start(self):
         self.round = 1
-        self.current_number = get_random_number()
+        self.current_number = self.get_random_number()
         say(f"The first number I'm thinking of is {self.current_number}.")
         sleep(0.5)
         say("Good luck.")
@@ -24,7 +31,7 @@ class Game:
 
     def _game_loop(self):
         self.last_number = self.current_number
-        self.current_number = get_random_number()
+        self.current_number = self.get_random_number()
 
         say(f"The next number is {self.current_number}.\nIs that higher, lower, or the same?")
         user_answer(self.get_choices())
@@ -47,19 +54,43 @@ class Game:
             play("success.wav")
             sleep(1)
             say("Correct!")
-
+            self.streak += 1
         else:
             play("failure.wav")
             sleep(1)
             say(f"Sorry. That's wrong. The first number was {self.last_number}, and the second was {self.current_number}.")
+            if self.streak > 0:
+                self.streak = 0
+            else:
+                self.streak -= 1
 
-        say(f'You have got {self.score} correct answers so far.')
+        incorrect = self.round - self.score
+        accuracy = self.get_accuracy()
+
+        if self.streak <= -2 and self.level > 1:
+            self.level -= 1
+            self.streak = 0
+        elif self.streak >= 5 and self.level < 5:
+            self.level += 1
+            self.streak = 0
+
+        _logger.debug(f"Game stats: Correct={self.score}, Incorrect={incorrect}, "
+                      f"Accuracy={accuracy}, Round={self.round}, Level={self.level}, Streak={self.streak}")
 
         self.round += 1
         self._game_loop()
 
+    def get_accuracy(self):
+        return self.score / self.round
+
     def end_game(self):
-        say(f"Okay. Let's stop. You scored {self.score} correct in {self.round - 1} rounds. Thanks for playing.")
+        say("Okay. Let's stop.")
+
+        if self.round > 2:
+            if self.score == self.round:
+                say(f"You got all questions correct. Well done.")
+            elif self.score > 1:
+                say(f"You scored {self.score} correct in {self.round - 1} rounds. Thanks for playing.")
 
 
     def get_choices(self):
@@ -72,7 +103,18 @@ class Game:
             choices[response] = self.same
         return choices
 
-def get_random_number():
-    return random.randrange(1, 100)
+    def get_random_number(self):
+        if self.level <= 1:
+            range = 10
+        elif self.level <= 3:
+            range = 20
+        elif self.level <= 4:
+            range = 50
+        elif self.level <= 5:
+            range = 100
+        else:
+            _logger.error(f"Unknown game level {self.level}")
+            range = 10
+        return random.randrange(1, range)
 
 
